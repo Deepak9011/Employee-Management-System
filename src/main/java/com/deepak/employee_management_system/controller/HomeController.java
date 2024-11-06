53f1333bf7a6df01df461a18305674909d090c7a
Deepak Agrawal <deepakagrawal9011@gmail.com>
2024-11-06 15:47:30 +0530

    version1.0

--- src/main/java/com/deepak/employee_management_system/controller/HomeController.java
+++ src/main/java/com/deepak/employee_management_system/controller/HomeController.java
@@ -1,4 +1,5 @@
 package com.deepak.employee_management_system.controller;
+import java.io.File;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +10,14 @@ public class HomeController {
     @RequestMapping("/")
 	public String index() {
         System.out.println("Hello");
+		// Create a File object for the current directory
+        File currentDir = new File(".");
+        
+        // Get the absolute path of the current directory
+        String currentDirectory = currentDir.getAbsolutePath();
+        
+        // Print the current directory
+        System.out.println("Current directory: " + currentDirectory);
 		return "home";
 	}
 
